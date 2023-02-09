8ea03b0c594b9e22ac0cb98eda7a2d79436a8700
Avitohol1 <87610719+Avitohol1@users.noreply.github.com>
2023-02-09 17:53:57 +0200

    added tick or cross icon next to answer that shows up after submitting

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,14 +1,17 @@
-import { nanoid } from "nanoid";
+import { nanoid } from "nanoid"
 import { decode } from 'html-entities'
 import './Question.css'
+import { useGlobalContext } from "../context"
+import {TiTick} from "react-icons/ti"
+import {ImCross} from "react-icons/im"
 
 const Question = (props) => {
+    const {isGameOver} = useGlobalContext()
 
     const incorrectAnswerElements = props.incorrectAnswers.map(incorrectAnswer => {
         const incorrectAnswerClassName = `
         ${props.selectedAnswer === incorrectAnswer ? "answer-button-selected" : "answer-button-default"}
-        ${(props.showAnswer && props.selectedAnswer === incorrectAnswer) && "question-button-incorrect"}
-        `
+        ${(props.showAnswer && props.selectedAnswer === incorrectAnswer) && "question-button-incorrect"}`
 
         return <button
                     key = {nanoid()}
@@ -23,27 +26,27 @@ const Question = (props) => {
         ${props.selectedAnswer === props.correctAnswer ? "answer-button-selected" : "answer-button-default"}
         ${(props.showAnswer && "question-button-correct")}`
 
-    const correctAnswerElement = 
-    <button 
+    const correctAnswerElement = <button 
         key = {nanoid()}
         className = {correctAnswerClassName}
         onClick = {() => props.handleSelectAnswer(props.id, props.correctAnswer)}
     >
-            {decode(props.correctAnswer)}
+        {decode(props.correctAnswer)}
     </button>
 
     incorrectAnswerElements.push(correctAnswerElement)
 
-    const shuffledElements = incorrectAnswerElements.sort((a, b) => (
+    const shuffledAnswers = incorrectAnswerElements.sort((a, b) => (
 		a.props.children.localeCompare(b.props.children))
-	);
+	)
 
     return ( 
         <div className='question-container'>
             <h3 className='question-text'>{props.number}{decode(props.question)}</h3>
-            {shuffledElements}
+            {shuffledAnswers}
+            {isGameOver && (props.selectedAnswer === props.correctAnswer ? <TiTick className="correct-icon" size={30}/> : <ImCross className="incorrect-icon" size={16}/>) }
         </div>
-    );
+    )
 }
  
-export default Question;
+export default Question
